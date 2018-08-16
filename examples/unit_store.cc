903374a10aeb1429e90bf64149a1567816adbe02
Constantine <solaris1987@sina.com>
2018-08-16 14:33:35 +0800

    change NetManager to SynManager

--- examples/unit_store.cc
+++ examples/unit_store.cc
@@ -18,10 +18,10 @@ int main(int argc, char**argv){
   google::InitGoogleLogging("Ambr");
   google::SetStderrLogging(google::GLOG_INFO);
   std::shared_ptr<ambr::store::StoreManager> store_manager = std::make_shared<ambr::store::StoreManager>();
-  std::shared_ptr<ambr::net::NetManager> net_manager = std::make_shared<ambr::net::NetManager>(store_manager);
+  std::shared_ptr<ambr::syn::SynManager> syn_manager = std::make_shared<ambr::syn::SynManager>(store_manager);
   //store_manager->Init("./unit");
   QApplication app(argc, argv);
-  StoreExampleMainWidget widget(store_manager, net_manager);
+  StoreExampleMainWidget widget(store_manager, syn_manager);
   widget.show();
   app.exec();
   return 0;
