c9a9b97ec88b3c4eaafb4ebbb203cede9d4705ca
lihn1987 <lihn1011@163.com>
2018-08-20 17:52:14 +0800

    add ReceiveUnit test for store

--- ut/cases/store.cc
+++ ut/cases/store.cc
@@ -102,10 +102,10 @@ TEST (UnitTest, Store) {
   ambr::core::Amount balance_used;
   ambr::core::Amount balance_remainder;
 
-  //==============================
-  //test send 1
+  //===last_unit_hash===========================
+  //test send 1----->SendToAddress
   {
-    //regular opration
+    //regular operate
     EXPECT_TRUE(manager->GetBalanceByPubKey(ambr::core::GetPublicKeyByPrivateKey(root_pri_key), balance_ori));
     for(int i = 0; i < 12; i++){
       balance_used = balance_used+send_ammout;
@@ -122,7 +122,7 @@ TEST (UnitTest, Store) {
     EXPECT_TRUE(manager->GetBalanceByPubKey(ambr::core::GetPublicKeyByPrivateKey(root_pri_key), balance_remainder));
     EXPECT_EQ(balance_remainder, balance_ori-balance_used);
 
-    //incorrect opration
+    //incorrect operate
     //error private key
     EXPECT_FALSE(manager->SendToAddress(test_pub, send_ammout, ambr::core::CreateRandomPrivateKey(), &test_hash, added_unit, &err));
     //error send_ammout(bigger than remainder)
@@ -132,7 +132,7 @@ TEST (UnitTest, Store) {
   //==============================
   //test send 2
   {
-    //regular opration
+    //regular operate------>AddSendUnit
     std::shared_ptr<ambr::core::SendUnit> send_unit = std::make_shared<ambr::core::SendUnit>();
     std::list<std::shared_ptr<ambr::store::UnitStore> > trade_history = manager->GetTradeHistoryByPubKey(ambr::core::GetPublicKeyByPrivateKey(root_pri_key), 10);
     send_unit->set_version((uint32_t)0x00000001);
@@ -140,11 +140,12 @@ TEST (UnitTest, Store) {
     send_unit->set_public_key(ambr::core::GetPublicKeyByPrivateKey(root_pri_key));
     send_unit->set_prev_unit(trade_history.front()->GetUnit()->hash());
     send_unit->set_balance(balance_remainder - 10000);
+    send_unit->set_dest(test_pub);
     send_unit->CalcHashAndFill();
     send_unit->SignatureAndFill(root_pri_key);
     EXPECT_TRUE(manager->AddSendUnit(send_unit, nullptr));
 
-    //incorrect opration
+    //incorrect operate
     send_unit->set_version((uint32_t)0x00000002);
     send_unit->CalcHashAndFill();
     send_unit->SignatureAndFill(root_pri_key);
@@ -190,10 +191,91 @@ TEST (UnitTest, Store) {
     send_unit->set_sign(ambr::core::Signature());
     EXPECT_FALSE(manager->AddSendUnit(send_unit, nullptr));
   }
-  {//test receive1
+  {//test receive1 ------>ReceiveFromUnitHash
+    //regular operate
     std::list<ambr::core::UnitHash> wait_list = manager->GetWaitForReceiveList(test_pub);
-    for(ambr::core::UnitHash hash: wait_list){
-
+    EXPECT_TRUE(wait_list.size() == 14);
+    for(size_t i = 0; i < 13; i++){
+      {//incorrect operate
+        EXPECT_FALSE(manager->ReceiveFromUnitHash("12345", test_pri, &test_hash, added_unit, &err));
+        EXPECT_FALSE(manager->ReceiveFromUnitHash(wait_list.front(), test_pri+1, &test_hash, added_unit, &err));
+      }
+      EXPECT_TRUE(manager->ReceiveFromUnitHash(wait_list.front(), test_pri, &test_hash, added_unit, &err));
+      wait_list.pop_front();
     }
+    ambr::core::Amount test_amount;
+    EXPECT_TRUE(manager->GetBalanceByPubKey(test_pub, test_amount));
+    EXPECT_TRUE(test_amount == balance_used);
+  }
+
+  {//test receive2 ------>AddReceiveUnit
+    ambr::core::UnitHash last_unit_hash;
+    ambr::core::Amount receive_amount;
+    ambr::core::UnitHash receive_from_hash;
+    ambr::core::Amount old_balance;
+
+    EXPECT_TRUE(manager->GetLastUnitHashByPubKey(test_pub, last_unit_hash));
+    std::list<ambr::core::UnitHash> wait_list = manager->GetWaitForReceiveList(test_pub);
+    EXPECT_TRUE(wait_list.size() == 1);
+    receive_from_hash = wait_list.front();
+    EXPECT_TRUE(manager->GetSendAmount(receive_from_hash, receive_amount, &err));
+    manager->GetBalanceByPubKey(test_pub, old_balance);
+
+    std::shared_ptr<ambr::core::ReceiveUnit> receive_unit = std::make_shared<ambr::core::ReceiveUnit>();
+
+    receive_unit->set_version(0x00000001);
+    receive_unit->set_type(ambr::core::UnitType::receive);
+    receive_unit->set_public_key(test_pub);
+    receive_unit->set_prev_unit(last_unit_hash);return;
+    receive_unit->set_balance(old_balance+receive_amount);
+    receive_unit->set_from(receive_from_hash);return;
+    receive_unit->CalcHashAndFill();
+    receive_unit->SignatureAndFill(test_pri);
+
+    {//incorrect operate
+      receive_unit->set_version(0x00000002);
+      EXPECT_FALSE(manager->AddReceiveUnit(receive_unit, &err));
+      receive_unit->set_version(0x00000001);
+
+      receive_unit->set_type(ambr::core::UnitType::send);
+      EXPECT_FALSE(manager->AddReceiveUnit(receive_unit, &err));
+      receive_unit->set_type(ambr::core::UnitType::receive);
+
+      receive_unit->set_public_key(test_pub+1);
+      EXPECT_FALSE(manager->AddReceiveUnit(receive_unit, &err));
+      receive_unit->set_public_key(test_pub);
+
+      receive_unit->set_prev_unit(last_unit_hash+1);
+      EXPECT_FALSE(manager->AddReceiveUnit(receive_unit, &err));
+      receive_unit->set_prev_unit(last_unit_hash);
+
+      receive_unit->set_balance(old_balance+receive_amount+1);
+      EXPECT_FALSE(manager->AddReceiveUnit(receive_unit, &err));
+      receive_unit->set_balance(old_balance+receive_amount-1);
+      EXPECT_FALSE(manager->AddReceiveUnit(receive_unit, &err));
+      receive_unit->set_balance(old_balance+receive_amount);
+
+      receive_unit->set_from(receive_from_hash-1);
+      EXPECT_FALSE(manager->AddReceiveUnit(receive_unit, &err));
+      receive_unit->set_from(receive_from_hash);
+
+      receive_unit->set_hash(receive_unit->hash()+1);
+      EXPECT_FALSE(manager->AddReceiveUnit(receive_unit, &err));
+      receive_unit->CalcHashAndFill();
+
+      receive_unit->SignatureAndFill(test_pri+1);
+      EXPECT_FALSE(manager->AddReceiveUnit(receive_unit, &err));
+      receive_unit->SignatureAndFill(test_pri);
+    }
+    EXPECT_TRUE(manager->AddReceiveUnit(receive_unit, &err));
+  }
+  {//test join validator set
+    ambr::core::PrivateKey validator_pri[10];
+    for(size_t i = 0; i < sizeof(validator_pri)/sizeof(ambr::core::PrivateKey); i++){
+      validator_pri[i] = ambr::core::CreateRandomPrivateKey();
+      EXPECT_TRUE(manager->SendToAddress(ambr::core::GetPublicKeyByPrivateKey(validator_pri[i]), 10000, root_pri_key, &test_hash, added_unit, &err));
+      EXPECT_TRUE(manager->ReceiveFromUnitHash(test_hash, validator_pri[i], &test_hash, added_unit, &err));
+    }
+    EXPECT_TRUE(manager->JoinValidatorSet(test_pri, 10000, &test_hash, added_unit, &err));
   }
 }
